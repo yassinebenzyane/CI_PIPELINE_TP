996d71987266f0d6ec45fa7a577c8f35d7fa5efb
Yassine <y.benzyane@edu.umi.ac.ma>
2026-03-02 14:13:00 +0000

    Initial commit: TP4 CI pipeline

---
+++ app/user_manager.py
@@ -0,0 +1,22 @@
+class UserManager:
+    def __init__(self):
+        self.users = []
+
+    def add_user(self, username: str):
+        if not username:
+            raise ValueError("Le nom d'utilisateur est obligatoire")
+        if username in self.users:
+            raise ValueError("Utilisateur déjà existant")
+        self.users.append(username)
+
+    def remove_user(self, username: str):
+        if username not in self.users:
+            raise ValueError("Utilisateur introuvable")
+        self.users.remove(username)
+
+    def count_users(self) -> int:
+        return len(self.users)
+
+
+def count_total_users(users) -> int:
+    return len(users)
